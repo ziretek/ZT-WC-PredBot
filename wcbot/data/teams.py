9efa4c244f335aec385c686aa059fb0bce98f19e
ziretek <zire@ziretek.com>
2026-06-24 19:30:23 +0100

    Add wcbot/data/teams.py (was missing from commit)

---
+++ wcbot/data/teams.py
@@ -0,0 +1,31 @@
+WORLD_CUP_TEAMS_2026 = [
+    "Argentina", "Australia", "Austria", "Belgium",
+    "Brazil", "Cameroon", "Canada", "Chile",
+    "Colombia", "Costa Rica", "Croatia", "Czech Republic",
+    "Denmark", "Ecuador", "Egypt", "England",
+    "France", "Germany", "Ghana", "Iran",
+    "Italy", "Ivory Coast", "Jamaica", "Japan",
+    "Mexico", "Morocco", "Netherlands", "New Zealand",
+    "Norway", "Paraguay", "Poland", "Portugal",
+    "Saudi Arabia", "Scotland", "Senegal", "Serbia",
+    "South Korea", "Spain", "Sweden", "Switzerland",
+    "Tunisia", "Turkey", "Ukraine", "United States",
+    "Uruguay", "Wales",
+]
+
+TEAM_CONTINENTS = {
+    "Argentina": "CONMEBOL", "Australia": "AFC", "Austria": "UEFA", "Belgium": "UEFA",
+    "Brazil": "CONMEBOL", "Cameroon": "CAF", "Canada": "CONCACAF", "Chile": "CONMEBOL",
+    "Colombia": "CONMEBOL", "Costa Rica": "CONCACAF", "Croatia": "UEFA", "Czech Republic": "UEFA",
+    "Denmark": "UEFA", "Ecuador": "CONMEBOL", "Egypt": "CAF", "England": "UEFA",
+    "France": "UEFA", "Germany": "UEFA", "Ghana": "CAF", "Iran": "AFC",
+    "Italy": "UEFA", "Ivory Coast": "CAF", "Jamaica": "CONCACAF", "Japan": "AFC",
+    "Mexico": "CONCACAF", "Morocco": "CAF", "Netherlands": "UEFA", "New Zealand": "OFC",
+    "Norway": "UEFA", "Paraguay": "CONMEBOL", "Poland": "UEFA", "Portugal": "UEFA",
+    "Saudi Arabia": "AFC", "Scotland": "UEFA", "Senegal": "CAF", "Serbia": "UEFA",
+    "South Korea": "AFC", "Spain": "UEFA", "Sweden": "UEFA", "Switzerland": "UEFA",
+    "Tunisia": "CAF", "Turkey": "UEFA", "Ukraine": "UEFA", "United States": "CONCACAF",
+    "Uruguay": "CONMEBOL", "Wales": "UEFA",
+}
+
+QUALIFIED_COUNT = 46
